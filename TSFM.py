35eec05d3789deabef3b18886898df9de310ab97
tiendatngcs <44583359+tiendatngcs@users.noreply.github.com>
2021-02-16 15:35:13 -0600

    Major changes to code structures, attributes, and methods.
    Add two series predictions, model and data getters. Update to plot function

--- TSFM.py
+++ TSFM.py
@@ -18,10 +18,9 @@ class TSFM(object):
                  date_variable: typing.Union[int, str],
                  value_variable: typing.Union[int, str],
                  target_variable: str,
-                 stop_date: str = None,         # stop date of train set, to split df to train and test sets
+                 stop_date: str,         # stop date of train set, to split df to train and test sets
                  section_list: list = None,
                  cycle_length: int = 12,
-                 do_anomaly_filter : bool = True,
                  alpha: float = 0.05,):
         self.target_variable = target_variable
         # self.n_pred_period = n_pred_period + abs((datetime.strptime(df[date_variable].to_numpy()[-1])  - datetime.strptime(stop_date, "%Y-%m-%d")).days)
@@ -44,6 +43,7 @@ class TSFM(object):
         # keys: sections(territories), value: list(train, test, pred), for easy storing and fetching data
         self.df_dict = dict()
         self.model_dict = dict()
+        self.adjusted_model_dict = dict()
 
         # Iterate through the unique sections
         self.section_list = section_list
@@ -51,15 +51,11 @@ class TSFM(object):
             self.section_list = df[target_variable].unique()
         for section in self.section_list:
             print("Inspecting", section, "...")
-            # Query data for one selected section
-            temp_train_df = self.get_train_data(section=section)
-            if temp_train_df.shape[0] >= 2 * cycle_length:
-                if do_anomaly_filter:
-                    temp_train_df = self.anomaly_filter(temp_train_df, alpha=alpha)
-                print("Training", temp_train_df.shape[0], "records ...")
-                # set Date as Index
-                # temp df now has 2 columns: section(territory) and value(order_volume)
-                arima_model = pmdarima.auto_arima(temp_train_df[temp_train_df.columns[-1]],
+            temp_actual_df = self.get_actual_data(section=section, is_adjusted=False)
+            if temp_actual_df.shape[0] >= 2 * cycle_length:
+                # train actual data
+                print("Training", temp_actual_df.shape[0], "actual records ...")
+                arima_model = pmdarima.auto_arima(temp_actual_df[temp_actual_df.columns[-1]],
                                                 start_p=0, start_P=0,
                                                 start_q=0, start_Q=0,
                                                 d=1, D=1,
@@ -68,84 +64,83 @@ class TSFM(object):
                                                 max_q=2, max_Q=2,
                                                 trace=True, m=cycle_length)
                 self.model_dict[section] = arima_model
+
+                temp_adjusted_actual_df = self.get_actual_data(section=section, is_adjusted=True)
+                # train adjusted actual data
+                print("Training", temp_adjusted_actual_df.shape[0], "adjusted actual records ...")
+                arima_model = pmdarima.auto_arima(temp_adjusted_actual_df[temp_adjusted_actual_df.columns[-1]],
+                                                start_p=0, start_P=0,
+                                                start_q=0, start_Q=0,
+                                                d=1, D=1,
+                                                max_p=4, max_P=2,
+                                                max_d=2, max_D=2,
+                                                max_q=2, max_Q=2,
+                                                trace=True, m=cycle_length)
+                self.adjusted_model_dict[section] = arima_model
             else:
                 print("Number of data points in Section", section, "is too small (" + str(
-                    temp_train_df.shape[0]) + ". Must be at least twice the declared cycle length.")
-    def get_actual_data(self, section: str):
+                    temp_actual_df.shape[0]) + ". Must be at least twice the declared cycle length.")
+
+    # DF Getters--------------------------------------------------------------
+    def get_actual_data(self, section: str, is_adjusted: bool = True,):
         agg_df = self.df[self.columns].groupby(self.columns[0:2], as_index=False).sum()
         agg_df = agg_df.query(self.columns[1] + "==" + "'" + section + "'")[[self.columns[0], self.columns[2]]]
         agg_df.set_index(self.columns[0], inplace=True)
         agg_df = TSFM.to_monthly(agg_df)
+        if is_adjusted:
+            return self.anomaly_filter(agg_df)
         return agg_df
-
-    def get_adjusted_actual_data(self, section: str):
-        train_df = self.get_train_data(section)
-        return self.anomaly_filter(self.get_actual_data(section), train_size=train_df.shape[0])
 
     def get_train_data(self, section: str,):  ## added stop date
         actual_df = self.get_actual_data(section)
-        if self.stop_date is None:
-            return actual_df
         return actual_df.iloc[lambda x: x.index <= self.stop_date]
 
     def get_test_data(self, section: str,):
         actual_df = self.get_actual_data(section)
-        if self.stop_date is None:
-            return actual_df
         return actual_df.iloc[lambda x: x.index > self.stop_date]
 
-    
-
-    def get_pred_data(self, section: str, return_conf_int: bool = False):
-        train_df = self.get_train_data(section)
-        model = self.model_dict[section]
-        pred, conf_int = model.predict(self.n_pred_period, return_conf_int=return_conf_int)
+    def get_pred_data(self, section: str, return_conf_int: bool = False, is_adjusted: bool = True):
+        actual_df = self.get_actual_data(section)
+        model = self.get_model(section=section, is_adjusted=is_adjusted)
+        print(model.predict(self.n_pred_period, return_conf_int=return_conf_int))
+        pred, conf_int = model.predict(self.n_pred_period, return_conf_int=True)
             
         # date_generator = DateGenerator(start_date=max(self.train[self.columns[0]]))
         temp_pred_df = pd.DataFrame(
             data={
-                self.columns[0]: pd.date_range(max(train_df.index),freq='MS',periods=self.n_pred_period+1)[1:],
+                self.columns[0]: pd.date_range(max(actual_df.index),freq='MS',periods=self.n_pred_period+1)[1:],
                 self.columns[1]: [section for x in range(len(pred))],
                 self.columns[-1]: pred})  # Use numbers inplace of future dates for now)
         self.pred = self.pred.append(temp_pred_df, ignore_index=True)
         temp_pred_df = temp_pred_df[[self.columns[0], self.columns[2]]]
         temp_pred_df.set_index(self.columns[0], inplace=True)
-        return temp_pred_df, conf_int
+        if return_conf_int:
+            return temp_pred_df, conf_int
+        return temp_pred_df
+
+    # Model Getters----------------------------------------------------------
+    def get_model(self, section: str, is_adjusted: bool = True):
+        if is_adjusted:
+            return self.adjusted_model_dict[section]
+        return self.model_dict[section]
     
+    # Plot Function-----------------------------------------------------------
     def plot(self, section: str):
-        actual = self.get_actual_data(section)
-        adjusted_actual = self.get_adjusted_actual_data(section)
-        pred, ci = self.get_pred_data(section, return_conf_int=True)
+        actual = self.get_actual_data(section, is_adjusted=False)
+        adjusted_actual, conf_int_df = self.anomaly_filter(actual, return_conf_int=True)
+        # pred, ci = self.get_pred_data(section, return_conf_int=True)
 
-        actual_model = pmdarima.auto_arima(actual[actual.columns[-1]],
-                                                start_p=0, start_P=0,
-                                                start_q=0, start_Q=0,
-                                                d=1, D=1,
-                                                max_p=4, max_P=2,
-                                                max_d=2, max_D=2,
-                                                max_q=2, max_Q=2,
-                                                trace=True, m=self.cycle_length)
-        adjusted_actual_model = pmdarima.auto_arima(adjusted_actual[adjusted_actual.columns[-1]],
-                                                start_p=0, start_P=0,
-                                                start_q=0, start_Q=0,
-                                                d=1, D=1,
-                                                max_p=4, max_P=2,
-                                                max_d=2, max_D=2,
-                                                max_q=2, max_Q=2,
-                                                trace=True, m=self.cycle_length)
-
-        actual_pred = actual_model.predict(12)
-        adjusted_actual_pred = adjusted_actual_model.predict(12)
-        pred_date_range = pd.date_range(max(actual.index),freq='MS',periods=12+1)[1:]
+        actual_pred = self.get_pred_data(section, is_adjusted=False)
+        adjusted_actual_pred = self.get_pred_data(section, is_adjusted=True)
 
         fig, ax = plt.subplots(figsize=(14,6))
         ax.plot(actual.index, actual[actual.columns[0]],label="Actual")   #Actuals This should come from original DS (all actuals)
         ax.plot(adjusted_actual.index, adjusted_actual[adjusted_actual.columns[0]],'-g', label="Adjusted Actual")   
-        ax.plot(pred.index, pred[pred.columns[0]], '-r',alpha=0.75,label="Forecast")  ## Pred
-        ax.fill_between(pred.index, ci[:, 0], ci[:, 1],alpha=0.3, color='b')  ## Conf intervals
+        # ax.plot(pred.index, pred[pred.columns[0]], '-r',alpha=0.75,label="Forecast")  ## Pred
+        ax.fill_between(conf_int_df.index, conf_int_df.iloc[:, 0], conf_int_df.iloc[:, 1],alpha=0.3, color='b')  ## Conf intervals
         
-        ax.plot(pred_date_range, actual_pred, '.b',alpha=0.75,label="Actual Forecast")
-        ax.plot(pred_date_range, adjusted_actual_pred, '.g',alpha=0.75,label="Adjusted Actual Forecast")
+        ax.plot(actual_pred.index, actual_pred.iloc[:, 0], '--b',alpha=0.75,label="Actual Forecast")
+        ax.plot(adjusted_actual_pred.index, adjusted_actual_pred.iloc[:, 0], '--g',alpha=0.75,label="Adjusted Actual Forecast")
         plt.title('Forecast Model')
         plt.xlabel('Year')
         plt.ylabel('Forecast Accurary')
@@ -157,99 +152,15 @@ class TSFM(object):
         plt.legend()
         plt.show()
 
-    # def is_anomalous(self, section: str, actual_value: float, date: datetime.datetime):
-    #     model = self.model_dict[section]
-    #     future_date_index = self.df_dict[section][0].index[-1].month - date.month
-    #     print("current_date = {}, pred_date = {}".format(self.df_dict[section][0].index[-1], date))
-    #     print([self.df_dict[section][0].shape[0] + future_date_index - 1])
-    #     pred = model.fit_predict([self.df_dict[section][0].shape[0] + future_date_index - 1])[-1]
-    #     alpha = 0.05
-    #     conf_int = model.conf_int(alpha = alpha)
-    #     extreme_conf_int = model.conf_int(alpha = 0.0005)
-
-    #     # zscore = (actual_value - forecast[0][-1]) / max(float(forecast[1][-1]), float_min)
-    #     # anomaly_probability = (2 * st.norm(0, 1).cdf(abs(zscore))) - 1
-        
-    #     result = {'Prediction': float(pred) if not float(
-    #                     pred) == float('inf') else 0.0,
-    #                 'CILower': float(conf_int[0]) if not float(
-    #                     conf_int[0]) == float('-inf') else 0.0,
-    #                 'CIUpper': float(conf_int[1]) if not float(
-    #                     conf_int[1]) == float('inf') else 0.0,
-    #                 'ConfLevel': float(1.0 - alpha) * 100,
-    #                 'IsAnomaly': actual_value < conf_int[0] or actual_value > conf_int[1],
-    #                 'IsAnomalyExtreme': actual_value < extreme_conf_int[0] or actual_value > extreme_conf_int[1],}
-    #                 # 'AnomalyProbability': 1 if raw_actual is None else float(anomaly_probability),
-    #                 # 'DownAnomalyProbability': 1 if raw_actual is None else float(down_anomaly_probability),
-    #                 # 'UpAnomalyProbability': 1 if raw_actual is None else float(up_anomaly_probability),
-    #                 # 'ModelFreshness': model_freshness}
-    #     print(result)
-    #     return(result)
-    
-    # def anomaly_filter(self, df: pd.core.frame.DataFrame, rolling_period: int = 12, train_size:int = 24, alpha: float = 0.05):
-    #     '''
-    #     input df is monthly
-    #     input df has date as index and 1 value column
-    #     '''
-    #     def find_anomaly_index(actual_list: list, conf_int_list: list, inf_bound: int = None):
-    #         print("finding Anomaly Index")
-    #         print(actual_list, conf_int_list)
-    #         for i in range(len(actual_list)):
-    #             actual = actual_list[i]
-    #             conf_int =  conf_int_list[i]
-    #             if inf_bound is not None:
-    #                 conf_int[inf_bound] = np.inf
-    #             if actual < conf_int[0] or actual > conf_int[1]:
-    #                 return i
-    #         return None
-
-    #     train = df.head(train_size)
-    #     inspecting_df = df.tail(df.shape[0] - train_size)
-        
-    #     while train.shape[0] < df.shape[0]:
-    #         arima_model = pmdarima.auto_arima(train[train.columns[0]],
-    #                                         start_p=0, start_P=0,
-    #                                         start_q=0, start_Q=0,
-    #                                         d=1, D=1,
-    #                                         max_p=4, max_P=2,
-    #                                         max_d=2, max_D=2,
-    #                                         max_q=2, max_Q=2,
-    #                                         trace=True, m=self.cycle_length)
-    #         pred_list, conf_int_list = arima_model.predict(n_periods=df.shape[0] - train.shape[0], return_conf_int=True, alpha=alpha)
-    #         anomaly_index = find_anomaly_index(df.iloc[train.shape[0]:][df.columns[0]], conf_int_list)
-    #         if anomaly_index is not None:
-    #             print("Anomaly spotted at {}".format(train.shape[0] + anomaly_index))
-    #             if anomaly_index != 0:
-    #                 train = train.append(df.iloc[train.shape[0]:train.shape[0] + anomaly_index])
-    #             arima_model = pmdarima.auto_arima(train[train.columns[0]],
-    #                                                 start_p=0, start_P=0,
-    #                                                 start_q=0, start_Q=0,
-    #                                                 d=1, D=1,
-    #                                                 max_p=4, max_P=2,
-    #                                                 max_d=2, max_D=2,
-    #                                                 max_q=2, max_Q=2,
-    #                                                 trace=True, m=self.cycle_length)
-    #             pred_list, conf_int_list = arima_model.predict(n_periods=1, return_conf_int=True, alpha=alpha)
-    #             pred, conf_int = pred_list[0], conf_int_list[0]
-    #             actual = df.iloc[train.shape[0]][df.columns[0]]
-    #             print("Pred {}, actual {}, conf_int {}".format(pred, actual, conf_int))
-    #             if actual < conf_int[0] or actual > conf_int[1]:
-    #                 if actual > conf_int[1]:
-    #                     print("Appending actual data")
-    #                     train = train.append(df.iloc[train.shape[0]])
-    #                 else:
-    #                     print("Appending modeled data")
-    #                     temp_pred_df = pd.DataFrame(
-    #                         data={train.columns[0]: [pred]}, index=pd.date_range(max(train.index),freq='MS',periods=2)[1:])
-    #                     train = train.append(temp_pred_df)
-    #         else:
-    #             print("No Anomaly spotted, appending the rest of the data")
-    #             train = train.append(df.iloc[train.shape[0]:])
-    #     return train
-
-    def anomaly_filter(self, df: pd.core.frame.DataFrame, n_rolling_period: int = 12, train_size:int = 24, alpha: float = 0.05):
-        train = df.head(train_size)
-        for i in range(train_size, df.shape[0], 12):
+    def anomaly_filter(self,
+                       df: pd.core.frame.DataFrame,
+                       return_conf_int: bool = False, 
+                       n_rolling_period: int = 12,
+                       alpha: float = 0.05):
+        train = df.iloc[lambda x: x.index <= self.stop_date]
+        stop_date = max(train.index)
+        returning_ic_list = list()
+        for i in range(train.shape[0], df.shape[0], 12):
             arima_model = pmdarima.auto_arima(train[train.columns[0]],
                                                 start_p=0, start_P=0,
                                                 start_q=0, start_Q=0,
@@ -266,6 +177,17 @@ class TSFM(object):
                 if temp_actual < ic[0] or temp_actual > ic[1]:
                     temp_actual_df.iloc[j, 0] = temp_pred[j]
             train = train.append(temp_actual_df)
+            returning_ic_list = returning_ic_list + ic_list.tolist()
+        if return_conf_int:
+            returning_ic_list = np.array(returning_ic_list)
+            ic_df = pd.DataFrame(
+                data={
+                    'lower': returning_ic_list[:, 0],
+                    'upper': returning_ic_list[:, 1],
+                },
+                index=pd.date_range(stop_date,freq='MS',periods=returning_ic_list.shape[0]+1)[1:]
+            )
+            return train, ic_df
         return train
 
 
